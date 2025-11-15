4cb8bd348e6d53d26760718fd7bcac35113430be
Mae-Meow <dobry.email@interia.pl>
2025-11-15 21:43:37 +0100

    waybar_setup.sh

--- waybar_setup.sh
+++ waybar_setup.sh
@@ -2,8 +2,9 @@
 
 mkdir -p ~/.config/waybar
 cp -r config.jsonc style.css ~/.config/waybar
+killall waybar && waybar
 
-echo "Yippie! It's installed and ready to go! Please relaunch Waybar to view the changes!"
+echo "Yippie! It's installed and ready to go!"
 
 cd --
 rmdir waybar-theme-mae
